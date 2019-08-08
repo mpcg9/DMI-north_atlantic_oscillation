9e415e5868af707b1710467218538f6cde49c218
Florian Sauerland <florian.sauerland@uni-bonn.de>
2019-08-08 11:18:27 +0200

    add file choosing dialog

--- io/readClimateData_test.m
+++ io/readClimateData_test.m
@@ -1,8 +1,10 @@
 clc, clear, close all;
 addpath(genpath(cd));
 addpath(genpath('../toolboxes'));
-data = readNetCDF2('../binary_data/NetCDF/ta_Amon_EC-Earth3_historical_r1i1p1f1_gr_201201-201212.nc');
+[file, path] = uigetfile('*.nc', 'Please select file to read');
+data = readNetCDF2(strcat(path,file));
 
+%% Find information
 disp(size(data.ta)); % To find out the dimensions of data.ta. 
 % This information can be used together with the dimension data of time,
 % latitude, longitude and elevation to find out which dimension of data.ta 
@@ -13,8 +15,10 @@ disp(size(data.ta)); % To find out the dimensions of data.ta.
 % <File>: [Order of dimensions], (dimension of dimensions)
 %
 % ta_Amon_EC-Earth3_historical_r1i1p1f1_gr_201201-201212.nc: [Lon, Lat, Elev, Time], (512, 256, 19, 12)
+% 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% Some more fun
 % Let's do a plot (just for testing)
 ta = data.ta(:,:,3,1)'; %January, Height 850hPa
 ta = [ta(:, 257:end), ta(:, 1:256)];

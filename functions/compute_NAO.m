fab1d554936552fd319d6c2ceba77a8cad2898fd
Florian Sauerland <florian.sauerland@uni-bonn.de>
2019-08-08 16:51:51 +0200

    changed box boundaries for NAO computation

--- functions/compute_NAO.m
+++ functions/compute_NAO.m
@@ -2,8 +2,10 @@ function [ NAO ] = compute_NAO( data )
 addpath(genpath('../toolboxes'));
 
 % cut out boxes
-iceland_box = select_subset(data, 50, 90, -45, 15);
-azores_box = select_subset(data, 20, 50, -60, 0);
+% iceland_box = select_subset(data, 50, 90, -45, 15);
+% azores_box = select_subset(data, 20, 50, -60, 0);
+iceland_box = select_subset(data, 55, 90, -80, 10);
+azores_box = select_subset(data, 30, 50, -80, 10);
 
 % Average pressure over box
 iceland_psi = mean(mean(iceland_box.psl, 1), 2);

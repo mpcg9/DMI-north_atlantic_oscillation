2e73f5c0081eb5d5f2928f269d47a54c6e346751
Florian Sauerland <florian.sauerland@uni-bonn.de>
2019-08-09 16:01:11 +0200

    sped up sorting procedure

--- io/scripts/sort_by_time.m
+++ io/scripts/sort_by_time.m
@@ -5,30 +5,35 @@ function [ data_output ] = sort_by_time( data )
 [data.time, time_idx] = sort(data.time);
 
 % copy data
-data_fields = fieldnames(data);
-l = length(data_fields);
-data_output = struct;
-for fieldnum = 1:l
-    fieldname = data_fields{fieldnum};
-    fieldval = getfield(data, fieldname);
-    if      strcmp(fieldname,'plev') ||...
-            strcmp(fieldname,'units') ||...
-            strcmp(fieldname,'lat') ||...
-            strcmp(fieldname,'lat_bnds') ||...
-            strcmp(fieldname,'lon') ||...
-            strcmp(fieldname,'lon_bnds')
-                data_output = setfield(data_output, fieldname, fieldval);
-    elseif  strcmp(fieldname, 'time')
-                data_output = setfield(data_output, fieldname, fieldval(time_idx));
-    elseif  strcmp(fieldname, 'time_bnds')
-                data_output = setfield(data_output, fieldname, fieldval(:,time_idx));
-    else
-                if length(size(fieldval)) == 4
-                    data_output = setfield(data_output, fieldname, fieldval(:, :, :, time_idx));
-                else
-                    data_output = setfield(data_output, fieldname, fieldval(:, :, time_idx));
-                end
+if nnz(time_idx' ~= 1:length(data.time)) > 0
+    data_fields = fieldnames(data);
+    l = length(data_fields);
+    data_output = struct;
+    for fieldnum = 1:l
+        fieldname = data_fields{fieldnum};
+        fieldval = getfield(data, fieldname);
+        if      strcmp(fieldname,'plev') ||...
+                strcmp(fieldname,'units') ||...
+                strcmp(fieldname,'lat') ||...
+                strcmp(fieldname,'lat_bnds') ||...
+                strcmp(fieldname,'lon') ||...
+                strcmp(fieldname,'lon_bnds')
+                    data_output = setfield(data_output, fieldname, fieldval);
+        elseif  strcmp(fieldname, 'time')
+                    data_output = setfield(data_output, fieldname, fieldval(time_idx));
+        elseif  strcmp(fieldname, 'time_bnds')
+                    data_output = setfield(data_output, fieldname, fieldval(:,time_idx));
+        else
+                    if length(size(fieldval)) == 4
+                        data_output = setfield(data_output, fieldname, fieldval(:, :, :, time_idx));
+                    else
+                        data_output = setfield(data_output, fieldname, fieldval(:, :, time_idx));
+                    end
+        end
     end
+else
+    disp('data already sorted, skipping...');
+    data_output = data;
 end
 end
 

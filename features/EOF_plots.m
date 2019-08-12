cfcd16f7429ba2e7e2c34d4fbfdc1d8a80e8094c
Florian Sauerland <florian.sauerland@uni-bonn.de>
2019-08-12 14:05:57 +0200

    add eigenvalues to plots

--- features/EOF_plots.m
+++ features/EOF_plots.m
@@ -14,8 +14,8 @@ folderContents = dir(strcat(path, '*.mat'));
 [s_file, s_path] = uiputfile({'.ps', 'Postscript Level 3 File (*.ps)'}, 'Save plots as...', strcat(path, 'plots.ps'));
 
 %% Settings
-Bounds_lat = [0, 90]; % Boundaries for latitude [in degrees]
-Bounds_lon = [-100, 20]; % Boundaries for longitude [in degrees]
+Bounds_lat = [30, 85]; % Boundaries for latitude [in degrees]
+Bounds_lon = [-80, 10]; % Boundaries for longitude [in degrees]
 Months = [1 1 0 0 0 0  0 0 0 0 0 1]; % Months to be evaluated [J F M A M J  J A S O N D] - Warning: only works if data starts on a January and stops on a December!
 noEOFs = 3; % Number of EOFs to calculate.
 averageData = true; % Set to true if you wish to calculate the averages for all grid points over time and substract it from the data
@@ -26,6 +26,7 @@ variableName = 'psl'; % Select variable
 
 %% EOF Calculation and Plotting
 noPlot = 1;
+tic;
 for i = 1:size(folderContents, 1)
     load(strcat(path,folderContents(i).name));
     
@@ -41,14 +42,24 @@ for i = 1:size(folderContents, 1)
     % Compute Singular Value Decomposition
     datasize = size(data.(variableName));
     data.(variableName) = reshape(data.(variableName), datasize(1)*datasize(2), datasize(3:end)); % Create observation matrix by reshaping
-    [U, S, V] = svds(data.(variableName)', noEOFs);
+    [~, S, V] = svds(data.(variableName)', noEOFs);
     
     % Change Latitude range to work with m_map
     temp_struct = struct;
     temp_struct.lon = data.lon;
     temp_struct.lat = data.lat;
+%     if plotEigenvalues
+%         unit = data.units;
+%         maxMultiplicator = max(abs(U*S),1)
+%     end
     clear data;
     [temp_struct, lon_idx] = convert_longitudes(temp_struct, min(Bounds_lon));
+    
+    % Compute Eigenvalues
+    if plotEigenvalues
+        eigenvalues = diag(S).^2./(size(S,1)-1);
+    end
+    clear U S;
     
     figure('Visible','off', 'Name', folderContents(i).name);
     for j = 1:noEOFs
@@ -65,10 +76,19 @@ for i = 1:size(folderContents, 1)
         m_image(temp_struct.lon, temp_struct.lat, temp_struct.z');
         m_coast('linewidth', 1, 'color', 'black');
         m_grid;
-        title(strcat(folderContents(i).name, ': EOF-', num2str(j)), 'Interpreter', 'none');
+        xlabel(...
+            {folderContents(i).name, ...
+            strcat('EOF-', num2str(j), '; Eigenvalue: ', num2str(eigenvalues(j))) ...
+            }, 'Interpreter', 'none');
         colorbar('southoutside'); 
         % You might want to add more things here for plotting
         
+    end
+    
+    if plotEigenvalues
+        subplot(ceil((noEOFs + plotEigenvalues)/2), 2, j+1);
+        semilogy(eigenvalues);
+        title('Eigenvalues');
     end
     
     % save plots to postscript file (can be easily converted to pdf, is the only format with -append option available)
@@ -79,3 +99,4 @@ for i = 1:size(folderContents, 1)
     end
     noPlot = noPlot + 1;
 end
+toc;

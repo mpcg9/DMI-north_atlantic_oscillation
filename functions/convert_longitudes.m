d0e4e90cbd7bd505609f4517b82bfa1ecfebdaf3
Florian Sauerland <florian.sauerland@uni-bonn.de>
2019-08-15 11:31:57 +0200

    improve function speed and stabilitz

--- functions/convert_longitudes.m
+++ functions/convert_longitudes.m
@@ -10,41 +10,51 @@ function [ data_converted, lon_idx] = convert_longitudes( data, lon_min )
 %   this_is_some_data(lon_idx, :, :, :) together with the output longitudes
 %   of this function).
 
+data_fields = fieldnames(data);
+for i = 1:length(data_fields)
+    if strcmp(data_fields{i},'lon') ||...
+       strcmp(data_fields{i},'longitude')
+        lonname = data_fields{i};
+    end
+end         
+
 lon_max = lon_min + 360;
-data.lon = mod(data.lon, 360);
+data.(lonname) = mod(data.(lonname), 360);
 
-too_large = data.lon >= lon_max;
+too_large = data.(lonname) >= lon_max;
 while nnz(too_large > 0)
-    data.lon(too_large) = data.lon(too_large) - 360;
-    too_large = data.lon >= lon_max;
+    data.(lonname)(too_large) = data.(lonname)(too_large) - 360;
+    too_large = data.(lonname) >= lon_max;
 end
-too_small = data.lon < lon_min;
+too_small = data.(lonname) < lon_min;
 while nnz(too_small > 0)
-    data.lon(too_small) = data.lon(too_small) + 360;
-    too_small = data.lon < lon_min;
+    data.(lonname)(too_small) = data.(lonname)(too_small) + 360;
+    too_small = data.(lonname) < lon_min;
 end
 
-[data.lon, lon_idx] = sort(data.lon);
+[data.(lonname), lon_idx] = sort(data.(lonname));
 
 data_fields = fieldnames(data);
 l = length(data_fields);
 data_converted = struct;
 for fieldnum = 1:l
     fieldname = data_fields{fieldnum};
-    fieldval = getfield(data, fieldname);
     if      strcmp(fieldname,'time') ||...
             strcmp(fieldname,'time_bnds') ||...
             strcmp(fieldname,'time_bounds') ||...
             strcmp(fieldname,'plev') ||...
             strcmp(fieldname,'units') ||...
             strcmp(fieldname,'lat') ||...
+            strcmp(fieldname,'latitude') ||...
             strcmp(fieldname,'lat_bnds') ||...
-            strcmp(fieldname,'lon') % This looks weird, but lon is already sorted above!
-                data_converted = setfield(data_converted, fieldname, fieldval);
-    elseif  strcmp(fieldname,'lon_bnds')
-                data_converted = setfield(data_converted, fieldname, fieldval(:,lon_idx));
+            strcmp(fieldname,'lat_bounds') ||...
+            strcmp(fieldname, lonname) % This looks weird, but lon is already sorted above!
+                data_converted.(fieldname) = data.(fieldname);
+    elseif  strcmp(fieldname,'lon_bnds') ||...
+            strcmp(fieldname,'lon_bounds')
+                data_converted.(fieldname) = data.(fieldname)(:,lon_idx);
     else
-                data_converted = setfield(data_converted, fieldname, fieldval(lon_idx, :, :, :));
+                data_converted.(fieldname) = data.(fieldname)(lon_idx, :, :, :);
     end
 end
 

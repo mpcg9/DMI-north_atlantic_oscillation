ef34ed9abb9a3c8cae40b05998a6a3601e40e380
Surmelann <urmelix@arcor.de>
2019-08-16 10:19:16 +0200

    renamed, added header

--- features/saveNAOsfromCMIP6.m
+++ features/saveNAOsfromCMIP6.m
@@ -1,17 +1,19 @@
-
+%% saveNAOsfromCMIP6
+% this script loads CMIP6 pressure data, computes the 'straighforward' NAO
+% as pressure differences between two boxes over iceland and the azores and
+% saves the results
+%
+% susann.aschenneller@uni-bonn.de, 08/2019
 %% settings
 clearvars; close all; clc;
 addpath(genpath(cd), genpath(['..' filesep 'data' filesep 'nao']),...
     genpath(['..' filesep 'functions']));
 
 %% load/save paths
-% [file, path] = uigetfile({'*.mat', 'MATLAB binary file (*.mat)'}, 'Please select one file to read. All files will be added automatically');
-file = 'psl_Amon_AWI-CM-1-1-MR_historical_r1i1p1f1_gn.mat';
-path = 'D:\cmip6\psl_NAO_EOF_historical\mat-files\';
+[file, path] = uigetfile({'*.mat', 'MATLAB binary file (*.mat)'}, 'Please select one file to read. All files will be added automatically');
 folderContents = dir(strcat(path, '*.mat'));
 
-% [~, s_path] = uiputfile({'.mat', 'MATLAB binary file (*.mat)'}, 'Save plots as...');
-s_path = 'D:\cmip6\psl_NAO_EOF_historical\mat-files\NAOs from pressure differences\';
+[~, s_path] = uiputfile({'.mat', 'MATLAB binary file (*.mat)'}, 'Save plots as...');
 
 %% load, compute NAO, save
 for i = 1:size(folderContents, 1)

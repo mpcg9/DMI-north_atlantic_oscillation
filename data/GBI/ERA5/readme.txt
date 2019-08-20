06daa574f6cef7014c98b7919843e71afe5cca78
Surmelann <urmelix@arcor.de>
2019-08-20 13:28:16 +0200

    added GBIs from CMIP6, NOAA and ERA5

---
+++ data/GBI/ERA5/readme.txt
@@ -0,0 +1,7 @@
+all .mat file have the boundaries:
+Bounds_lat = [60 80]; % Boundaries for latitude [in degrees]
+Bounds_lon = [-80 -20]; % Boundaries for longitude [in degrees]
+
+geopotential converted to unit 'meters' via dividing by gravitational acceleration (see convert_gpm_to_meters.m)
+
+GBI calculated with compute_GBI.m
---
+++ features/saveGBIsfromCMIP6.m
@@ -0,0 +1,28 @@
+%% saveGBIsfromCMIP6
+% this script loads CMIP6 geopotential data, computes the GBI and saves the
+% results
+%
+% susann.aschenneller@uni-bonn.de, 08/2019
+
+%% settings
+clearvars; clc;
+addpath(genpath(cd), genpath(['..' filesep 'functions']));
+
+%% load/save paths
+[file, path] = uigetfile({'*.mat', 'MATLAB binary file (*.mat)'}, 'Please select one file to read. All files will be added automatically');
+folderContents = dir(strcat(path, '*.mat'));
+
+[~, s_path] = uiputfile({'.mat', 'MATLAB binary file (*.mat)'}, 'Save plots as...');
+
+%% load, compute NAO, save
+for i = 1:size(folderContents, 1)
+    load(strcat(path,folderContents(i).name));
+    GBI_temp = compute_GBI(data);
+    
+    data = convert_times(data);
+    data = setfield(data,'time',datetime(data.time,'Format','dd.MM.yyyy'));
+    
+    GBI = struct('time',data.time);
+    GBI = setfield(GBI,'GBI',GBI_temp);
+    save(strcat(s_path, 'GBI_', folderContents(i).name),'GBI');
+end

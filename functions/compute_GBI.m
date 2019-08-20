9df1926fa3c1494593c3468f1259d50af424cbb0
Surmelann <urmelix@arcor.de>
2019-08-20 16:09:27 +0200

    corrected gbi computation

--- functions/compute_GBI.m
+++ functions/compute_GBI.m
@@ -2,9 +2,9 @@ function [ GBI ] = compute_GBI( data )
 
 varn = getVariableName(data);
 data = select_subset(data, 60, 80, -80, -20);
-if varn ~= 'zg'
-    warning('Make sure to use variable zg for GBI calculation');
-end
+% if varn ~= 'zg'
+%     warning('Make sure to use variable zg for GBI calculation');
+% end
 
 % Find Fieldnames
 data_fields = fieldnames(data);
@@ -19,9 +19,9 @@ for i = 1:length(data_fields)
 end
 
 weights = cos(data.(latname) .* (pi/180));
-weights = weights ./ norm(weights);
+weights = weights ./ sum(weights);
 % data.(varn) = data.(varn) - mean(data.(varn), 3);
-GBI = reshape(mean(mean(data.(varn) .* weights')), size(data.(varn), 3), 1);
+GBI = reshape(mean(sum(data.(varn) .* weights', 2)), size(data.(varn), 3), 1);
 
 end
 

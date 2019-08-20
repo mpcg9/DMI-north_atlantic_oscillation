5347b5cbeb8d8d137f444978789f372330d5ef1c
Surmelann <urmelix@arcor.de>
2019-08-20 12:40:50 +0200

    GBI from ERA5, conversion gpm to meters

--- io/readClimateData.m
+++ io/readClimateData.m
@@ -1,5 +1,6 @@
 clc, clear, close all;
 [file, path] = uigetfile('*.nc', 'Please select file to read');
 addpath('./scripts');
-data = readNetCDF2_new(strcat(path,file),'Latitudes', [60 80],'Longitudes', [-80 -20]);
+% data = readNetCDF2_new(strcat(path,file),'Latitudes', [60 80],'Longitudes', [-80 -20]);
+data = readNetCDF2_new(strcat(path,file));
 uisave('data', file);

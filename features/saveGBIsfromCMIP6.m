8ceee6128b85df0b24b9418e25826fd5640f02da
Surmelann <urmelix@arcor.de>
2019-08-20 16:08:45 +0200

    minor changes

--- features/saveGBIsfromCMIP6.m
+++ features/saveGBIsfromCMIP6.m
@@ -9,7 +9,9 @@ clearvars; clc;
 addpath(genpath(cd), genpath(['..' filesep 'functions']));
 
 %% load/save paths
-[file, path] = uigetfile({'*.mat', 'MATLAB binary file (*.mat)'}, 'Please select one file to read. All files will be added automatically');
+% [file, path] = uigetfile({'*.mat', 'MATLAB binary file (*.mat)'}, 'Please select one file to read. All files will be added automatically');
+path = 'D:\cmip6\zg_GBI_historical\mat-files\';
+file = 'zg_Amon_AWI-CM-1-1-MR_historical_r1i1p1f1_gn.mat';
 folderContents = dir(strcat(path, '*.mat'));
 
 [~, s_path] = uiputfile({'.mat', 'MATLAB binary file (*.mat)'}, 'Save plots as...');

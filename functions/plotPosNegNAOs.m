dfac8022247bdb69a45dcd196c9c486ee383b1ea
Surmelann <urmelix@arcor.de>
2019-08-27 17:30:20 +0200

    corrected the classical plotfunction for all posneg NAOs

--- functions/plotPosNegNAOs.m
+++ functions/plotPosNegNAOs.m
@@ -1,30 +1,28 @@
 function [] = plotPosNegNAOs(time_np,size,negpos)
 % [] = plotPosNegNAOs(time_np,size,negpos)
-% that works also with 'bar', but takes much more time.
-
-timediff = diff(month(time_np));
+% creates a stem-like plot of positive/negative NAOs over time, that have
+% all the same length
+% INPUT
+%   time_np     -   datetime vector with the dates of positive/negative NAO
+%   size        -   desired length of the stems
+%   negpos      -   'neg' for negative NAO, 'pos' for positive NAO
+%
+% susann.aschenneller@uni-bonn.de
 
 % length and color of the bars
+num = length(time_np);
 if negpos == 'neg'
-    yvalues = -size*(ones(2,1));
+    yvalues = -size*(ones(num,1));
     color = 'b';
 elseif negpos == 'pos'
-    yvalues = size*(ones(2,1));
+    yvalues = size*(ones(num,1));
     color = 'r';
 end
 
-for k = 2 : length(timediff)
-    if timediff(k-1) == 1 || timediff(k-1) == -11
-        temp(1,1) = time_np(k-1);
-        temp(2,1) = time_np(k);
-        area(temp,yvalues,'FaceColor',color,'EdgeColor',color);
-    end
-end
+bar(time_np,yvalues,'FaceColor',color);
 
 % axis limitations
-x_min3 = time_np(1); x_max3 = time_np(end);
 y_min3 = -2 * size; y_max3 = 2 * size;
-xlim([x_min3 x_max3]);
 ylim([y_min3 y_max3]);
 
 end

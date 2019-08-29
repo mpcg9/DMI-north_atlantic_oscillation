e2ca390cbd993c6de1f580518cf263f6e159ef35
Surmelann <urmelix@arcor.de>
2019-08-29 12:22:21 +0200

    worked on corrections/reductions

--- features/compareGBIs.m
+++ features/compareGBIs.m
@@ -26,8 +26,8 @@ addpath(genpath(cd), genpath(['..' f 'functions']), genpath(['..' f 'data' f 'GB
 
 % Plot or not
 plot_historical = true;          % plot historical data
-plot_future = true;              % plot historical data from future scenarios SSP245 and SSP585
-plot_stdev = true;               % running standard deviation
+plot_future = false;              % plot historical data from future scenarios SSP245 and SSP585
+plot_stdev = false;               % running standard deviation
 plot_historicalAndFuture = true; % creates a 'messy-plot'
 
 %% 1. Load data
@@ -75,11 +75,35 @@ for i = 1 : size(folderContents, 1)
     gbi_CMIP6_scen585{i}.name = erase(gbi_CMIP6_scen585{i}.name,'GBI zg Amon ');
 end
 
-%% 2. Todo: Correct future data by 
-    % - seasonal mean (e.g. 12-months-filter)
-    % - warming effect --> subtract mean over all longitudes
-%% 2.1 Computation of zonal mean
-% (temporal mean of the data over all longitudes in the specified latitudes)  
+%% 2. Corrections / Reductions
+
+%% 2.x Monthly means of daily NOAA data
+gbi_NOAA_mon = meanFilter(gbi_NOAA,30);
+   
+%% 2.x Reduction for seasonal/annual cycle (?) 12-months-filter
+a = 1;
+% window size for monthly data
+ws = 12; % [months]
+b = (1/ws)*ones(1,ws);
+gbi_NOAA_filt = filter(b,a,gbi_NOAA_mon.GBI);
+gbi_ERA5_filt = filter(b,a,gbi_ERA5.GBI);
+
+for k = 1 : length(gbi_CMIP6_hist)
+    gbi_CMIP6_hist_filt{k} = filter(b,a,(gbi_CMIP6_hist{k}.GBI));
+end
+
+for k = 1 : length(gbi_CMIP6_scen245)
+    gbi_CMIP6_scen245_filt{k} = filter(b,a,(gbi_CMIP6_scen245{k}.GBI));
+end
+
+for k = 1 : length(gbi_CMIP6_scen585)
+    gbi_CMIP6_scen585_filt{k} = filter(b,a,(gbi_CMIP6_scen585{k}.GBI));
+end
+
+%% 2.x Computation of zonal mean
+% (temporal mean of the data over all longitudes in the specified latitudes)
+% for correction of changes only because of warming in the future scenarios
+% one mean-value for each model
 
 % CMIP6 SSP245
 path = '..\data\GBI\CMIP6_zg_ssp245\';
@@ -109,22 +133,17 @@ for k = 1 : length(gbi_zonal_CMIP6_scen585)
    gbi_zonal_mean_CMIP6_scen585(k) = mean(gbi_zonal_CMIP6_scen585{k}.GBI);
 end
 
-%% 3. Filtering
-a = 1;
-% window size for daily data
-ws_filt = 36;
-b = (1/ws_filt)*ones(1,ws_filt);
+%% 2.x Subtraction of zonal mean from annual-filtered data
 
-% compute filtered time series
-gbi_NOAA_filt = filter(b,a,gbi_NOAA.GBI);
 
 %% 4. Running standard deviation
+% xxx think again about windows size
 % window size
 ws_std_daily = 60 * 30; % [days]
 ws_std_mon = 60; % [months]
 
-gbi_NOAA_stdev = movstd(gbi_NOAA.GBI,ws_std_daily);
-gbi_ERA5_stdev = movstd(gbi_ERA5.GBI,ws_std_mon);
+gbi_NOAA_stdev = movstd(gbi_NOAA_filt,ws_std_daily);
+gbi_ERA5_stdev = movstd(gbi_ERA5_filt,ws_std_mon);
 
 % moving standard deviations
 for k = 1 : length(gbi_CMIP6_hist)
@@ -152,6 +171,7 @@ colors.IndianRed = [255,106,106] ./ 255;
 colors.turquoise4 = [0,134,139] ./ 255;
 colors.blue = [0 0 1];
 colors.red = [1 0 0];
+colors.grey = [102,102,102] ./ 255;
 
 % this is for manual line specifications in a for-loop
 line_spec{1,1} = colors.blue;
@@ -184,39 +204,41 @@ lw1 = 1.2;
 if plot_historical == true
     % axis settings
     x_min = datetime(1979,1,1); x_max = datetime(2019,7,1);
-    y_min = 4800; y_max = 5800;  
+    y_min = 5150; y_max = 5410;  
     
     % --- Part 1 ---
     figure('units','normalized','outerposition',[0 0 1 1]); grid on; hold on;
     % references
-    plot(gbi_NOAA.time,gbi_NOAA_filt,'k','DisplayName',...
-        ['NOAA (daily) download, filtered with windows size ' num2str(ws_filt)]);
-    plot(gbi_ERA5.time,gbi_ERA5.GBI,'DisplayName','ERA5 (monthly)');
+    plot(gbi_NOAA_mon.time,gbi_NOAA_filt,'Color',colors.grey,'LineWidth',lw1,...
+        'DisplayName',['NOAA (daily) download, monthly means']);
+    plot(gbi_ERA5.time,gbi_ERA5_filt,'k','LineWidth',lw1,...
+        'DisplayName','ERA5 (monthly)');
     
     % plot first 5 historical models individually
     for k = 1 : 5
-        plot(gbi_CMIP6_hist{k}.time,gbi_CMIP6_hist{k}.GBI,...
+        plot(gbi_CMIP6_hist{k}.time,gbi_CMIP6_hist_filt{k},'LineWidth',lw1,...
             'DisplayName',['CMIP6 historical ' gbi_CMIP6_hist{k}.name]);
     end
     
     xlim([x_min x_max]); ylim([y_min y_max]); legend show
-    title('GBI as a weighted mean of the geopotential height at 500hPa pressure level part 1');
+    title('GBI as a weighted mean of the geopotential height at 500hPa pressure level, 12-months-filter, part 1');
     ylabel('GBI [m]'); hold off;
     
     % --- Part 2 ---
     figure('units','normalized','outerposition',[0 0 1 1]); grid on; hold on;
     % references
-    plot(gbi_NOAA.time,gbi_NOAA_filt,'k','DisplayName',...
-        ['NOAA (daily) download, filtered with windows size ' num2str(ws_filt)]);
-    plot(gbi_ERA5.time,gbi_ERA5.GBI,'DisplayName','ERA5 (monthly)');
+    plot(gbi_NOAA_mon.time,gbi_NOAA_filt,'Color',colors.grey,'LineWidth',lw1,...
+        'DisplayName',['NOAA (daily) download, monthly means']);
+    plot(gbi_ERA5.time,gbi_ERA5_filt,'k','LineWidth',lw1,...
+        'DisplayName','ERA5 (monthly)');
     
     % plot second 5 historical models individually
-    for k = 6 : length(gbi_CMIP6_hist)
-        plot(gbi_CMIP6_hist{k}.time,gbi_CMIP6_hist{k}.GBI,...
+    for k = 6 : length(gbi_CMIP6_hist_filt)
+        plot(gbi_CMIP6_hist{k}.time,gbi_CMIP6_hist_filt{k},'LineWidth',lw1,...
             'DisplayName',['CMIP6 historical ' gbi_CMIP6_hist{k}.name]);
     end
     xlim([x_min x_max]); ylim([y_min y_max]); legend show
-    title('GBI as a weighted mean of the geopotential height at 500hPa pressure level part 2');
+    title('GBI as a weighted mean of the geopotential height at 500hPa pressure, 12-months-filter, level part 2');
     ylabel('GBI [m]'); hold off;    
 end
 %% 5.3 Historical simulation AND future scenarios 
